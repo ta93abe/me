e103fbfa1f045be8b9ce293330e0c8602b5c6e4e
Takumi Abe <ta93abe@ta93abe.com>
2024-01-14 14:43:27 +0900

    playwright setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      {children}
-      <Analytics/>
-      <SpeedInsights/>
+        {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
